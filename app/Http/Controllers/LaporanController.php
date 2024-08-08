7add8eac7a251bd35c9108af934092fead67778e
rayys0703 <rayys0703@gmail.com>
2024-08-08 11:05:45 +0700

    Fix bug di Laporan Stok Barang

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -13,71 +13,53 @@ class LaporanController extends Controller
 {
 
     public function stok(Request $request)
-    {
-        $search = $request->input('search');
+	{
+		$search = $request->input('search');
+        $startDate = $request->input('start_date');
+        $endDate = $request->input('end_date');
 
-        $query = DB::table('barang_masuk')
-            ->leftJoin('barang', 'barang_masuk.barang_id', '=', 'barang.id')
-            ->leftJoin('jenis_barang', 'barang.jenis_barang_id', '=', 'jenis_barang.id')
-            ->leftJoin('supplier', 'barang.supplier_id', '=', 'supplier.id')
-            ->leftJoin('detail_barang_masuk', 'barang.supplier_id', '=', 'detail_barang_masuk.id')
-            ->leftJoin('status_barang', 'barang_masuk.status_barang_id', '=', 'status_barang.id')
-            ->select(
-                'barang_masuk.barang_id',
-                'barang.id as valbarang_id',
-                'barang.nama as nama_barang',
-                'barang.keterangan as keterangan_barang',
-                'jenis_barang.nama as nama_jenis_barang',
-                'supplier.nama as supplier_name',
-                'status_barang.nama as status_name',
-                'status_barang.warna as warna_status',
-                DB::raw('SUM(barang_masuk.jumlah) as jumlah')
-            )
-            ->where('jumlah', '>', 0)
-            ->when($search, function ($query) use ($search) {
-                return $query->where('barang.nama', 'like', '%' . $search . '%')
-                    ->orWhere('jenis_barang.nama', 'like', '%' . $search . '%');
+		$data = DB::table('barang_masuk')
+			->leftJoin('barang', 'barang_masuk.barang_id', '=', 'barang.id')
+			->leftJoin('supplier', 'barang.supplier_id', '=', 'supplier.id')
+			->leftJoin('jenis_barang', 'barang.jenis_barang_id', '=', 'jenis_barang.id')
+			->select(
+				'barang_masuk.id as barang_masuk_id',
+				'barang_masuk.keterangan',
+				'barang_masuk.tanggal',
+				'barang.nama as nama_barang',
+				'jenis_barang.nama as nama_jenis_barang',
+				'supplier.nama as nama_supplier',
+				'barang_masuk.jumlah'
+			)
+			->selectRaw("DATE_FORMAT(barang_masuk.tanggal, '%d %M %Y') as formatted_tanggal")
+			->when($search, function ($query) use ($search) {
+				return $query->where('barang_masuk.barang_id', 'like', '%' . $search . '%')
+					->orWhere('barang.nama', 'like', '%' . $search . '%')
+					->orWhere('supplier.nama', 'like', '%' . $search . '%');
+			})
+            ->when($startDate && $endDate, function ($query) use ($startDate, $endDate) {
+                return $query->whereBetween('barang_masuk.tanggal', [$startDate, $endDate]);
             })
-            ->orderBy('jumlah','desc')
-            ->orderBy('barang.nama','asc');
+			->orderBy('barang_masuk.created_at', 'desc')
+			->paginate(7);
 
-        $data = $query->get();
+		foreach ($data as $item) {
+			$item->detail = DB::table('detail_barang_masuk')
+				->leftJoin('serial_number', 'detail_barang_masuk.serial_number_id', '=', 'serial_number.id')
+				->leftJoin('status_barang', 'detail_barang_masuk.status_barang_id', '=', 'status_barang.id')
+				->select('serial_number.serial_number', 'status_barang.nama as status_barang', 'detail_barang_masuk.kelengkapan')
+				->where('detail_barang_masuk.barangmasuk_id', $item->barang_masuk_id)
+				->orderBy('serial_number.serial_number', 'asc')
+				->get();
+		}
 
-        // Memproses data ke grup Serial Number berdasarkan Supplier dan Kondisi Barang
-        $groupedData = $data->groupBy('barang_id')->map(function ($items) {
-            $result = [
-                'barang_id' => $items->first()->barang_id,
-                'nama_barang' => $items->first()->nama_barang,
-                'nama_jenis_barang' => $items->first()->nama_jenis_barang,
-                'jumlah_barang' => $items->first()->jumlah_barang,
-                'keterangan_barang' => $items->first()->keterangan_barang,
-                'suppliers' => []
-            ];
+		$data->getCollection()->transform(function ($item) {
+			$item->tanggal = \Carbon\Carbon::parse($item->tanggal)->isoFormat('DD MMMM YYYY');
+			return $item;
+		});
 
-            foreach ($items as $item) {
-                if (!isset($result['suppliers'][$item->supplier_name])) {
-                    $result['suppliers'][$item->supplier_name] = [];
-                }
-                $result['suppliers'][$item->supplier_name][] = [
-                    'serial_number' => $item->serial_number,
-                    'nama_status' => $item->status_name,
-                    'warna_status' => $item->warna_status
-                ];
-            }
-
-            return $result;
-        });
-
-        $data = new \Illuminate\Pagination\LengthAwarePaginator(
-            $groupedData->forPage(\Illuminate\Pagination\Paginator::resolveCurrentPage(), 7),
-            $groupedData->count(),
-            7,
-            null,
-            ['path' => \Illuminate\Pagination\Paginator::resolveCurrentPath()]
-        );
-
-        return view('laporan.stok.index', ['data' => $data]);
-    }
+		return view('laporan.stok.index', compact('data', 'startDate', 'endDate'));
+	}
 
     public function barangmasuk(Request $request)
     {
--- resources/views/laporan/stok/index.blade.php
+++ resources/views/laporan/stok/index.blade.php
@@ -34,125 +34,117 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 @if (!$data->isEmpty())
-                    <table class="w-full text-sm text-left rtl:text-right text-gray-500">
-                        <thead class="text-xs text-gray-700 uppercase bg-white">
-                            <tr>
-                                <th scope="col" class="px-6 py-3">
-                                    No
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Nama Barang
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Jenis Barang
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Jumlah
-                                </th>
-                                {{-- <th scope="col" class="px-6 py-3">
-                                    Kondisi
-                                </th> --}}
-                                <th scope="col" class="px-6 py-3">
-                                    Keterangan
-                                </th>
-                                <th scope="col" class="px-6 py-3">Aksi</th>
+                <!-- Cari berdasarkan Tanggal -->
+                <form action="{{ route('laporan.stok.index') }}" method="GET" class="flex justify-end py-3 px-6 mb-4 border-1 border-b-2">
+                    <div class="flex items-center space-x-3">
+                        <div id="date-range-picker" date-rangepicker class="flex items-center">
+                            <style>input[type="date"]::-webkit-inner-spin-button,
+                            input[type="date"]::-webkit-calendar-picker-indicator {opacity:0;}</style>
+                            <span class="me-4 text-gray-500">dari</span>
+                            <div class="relative">
+                                <div class="absolute inset-y-0 end-0 flex items-center pe-3 pointer-events-none">
+                                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                        <path
+                                            d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                                    </svg>
+                                </div>
+                                <input id="datepicker-range-start" name="start_date" type="date"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-2xl focus:ring-blue-500 focus:border-blue-500 block w-full pe-2.5 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="Select date start" value="{{ $startDate ?? '' }}">
+                            </div>
+                            <span class="mx-4 text-gray-500">hingga</span>
+                            <div class="relative">
+                                <div class="absolute inset-y-0 end-0 flex items-center pe-3 pointer-events-none">
+                                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                        <path
+                                            d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                                    </svg>
+                                </div>
+                                <input id="datepicker-range-end" name="end_date" type="date"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-2xl focus:ring-blue-500 focus:border-blue-500 block w-full pe-2.5 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="Select date end" value="{{ $endDate ?? '' }}">
+                            </div>
+                        </div>
+                        <div class="flex items-end">
+                            <button type="submit"
+                                class="bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 text-white font-medium py-2 px-3 rounded-2xl">
+                                Cari
+                            </button>
+                        </div>
+                    </div>
+                </form>
+                <!-- Tabel -->
+                <table class="w-full text-sm text-left rtl:text-right text-gray-500">
+                    <thead class="text-xs text-gray-700 uppercase bg-white">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">No</th>
+                            <th scope="col" class="px-6 py-3">Barang</th>
+                            <th scope="col" class="px-6 py-3">Stok</th>
+                            <th scope="col" class="px-6 py-3">Tanggal</th>
+                            <th scope="col" class="px-6 py-3">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($data as $d)
+                            <tr class="bg-white border-b hover:bg-gray-50 text-base text-black">
+                                <td class="w-4 px-6 py-4">
+                                    <span class="flex justify-center items-center">{{ $loop->iteration }}</span>
+                                </td>
+                                <td class="px-6 py-4">{{ $d->nama_barang }}</td>
+                                <td class="px-6 py-4">{{ $d->jumlah ?: 0 }}</td>
+                                <td class="px-6 py-4">{{ $d->tanggal }}</td>
+                                <td class="px-6 py-4 flex gap-x-2">
+                                    <button type="button"
+                                        class="flex text-white bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-2xl text-sm px-3 py-1.5 text-center"
+                                        data-bs-toggle="modal" data-bs-target="#detailModal{{ $d->barang_masuk_id }}">
+                                        <!-- SVG -->
+                                    </button>
+                                </td>
                             </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($data as $d)
-                                <tr class="bg-white border-b hover:bg-gray-50">
-                                    <td class="w-4 p-4">
-                                        <div class="flex items-center">
-                                            <div class="ps-3 w-4 font-medium text-gray-900">
-                                                {{ $loop->iteration }}
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <th scope="row"
-                                        class="flex items-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                                        <div class="ps-0">
-                                            <div class="text-base font-semibold">{{ $d['nama_barang'] }}</div>
-                                        </div>
-                                    </th>
-                                    <td class="px-6 py-4">
-                                        <div class="text-base text-black">{{ $d['nama_jenis_barang'] }}</div>
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        <div class="text-base text-black">{{ $d['jumlah_barang'] }}</div>
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        <div class="text-base text-black">{{ $d['keterangan_barang'] }}</div>
-                                    </td>
-                                    <td class="px-6 py-4 flex gap-x-2">
-                                        <button type="button"
-                                            class="flex text-white bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-2xl text-sm px-3 py-1.5 text-center"
-                                            data-bs-toggle="modal" data-bs-target="#detailModal{{ $d['barang_id'] }}">
-                                            <svg class='line' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'
-                                                stroke="white" stroke-width="2" fill="none" width="18"
-                                                height="18">
-                                                <g transform='translate(3.649800, 2.749900)'>
-                                                    <line x1='10.6555' y1='12.6999' x2='5.2555' y2='12.6999'>
-                                                    </line>
-                                                    <line x1='8.6106' y1='8.6886' x2='5.2546' y2='8.6886'>
-                                                    </line>
-                                                    <path
-                                                        d='M16.51,5.55 L10.84,0.15 C10.11,0.05 9.29,0 8.39,0 C2.1,0 -1.95399252e-14,2.32 -1.95399252e-14,9.25 C-1.95399252e-14,16.19 2.1,18.5 8.39,18.5 C14.69,18.5 16.79,16.19 16.79,9.25 C16.79,7.83 16.7,6.6 16.51,5.55 Z'>
-                                                    </path>
-                                                    <path
-                                                        d='M10.2844,0.0827 L10.2844,2.7437 C10.2844,4.6017 11.7904,6.1067 13.6484,6.1067 L16.5994,6.1067'>
-                                                    </path>
-                                                </g>
-                                            </svg>
-                                        </button>
-                                    </td>
-                                </tr>
 
-                                <!-- Modal -->
-                                <div class="modal fade" id="detailModal{{ $d['barang_id'] }}" tabindex="-1"
-                                    aria-labelledby="detailModalLabel{{ $d['barang_id'] }}" aria-hidden="true">
-                                    <div class="modal-dialog">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title" id="detailModalLabel{{ $d['barang_id'] }}">
-                                                    Detail Barang
-                                                </h5>
-                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                    aria-label="Close"></button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <div class="grid grid-cols-10 gap-2">
-                                                    <div class="font-bold col-span-3">Supplier,<br>SN, Kondisi:</div>
+                            <div class="modal fade" id="detailModal{{ $d->barang_masuk_id }}" tabindex="-1" aria-labelledby="detailModalLabel{{ $d->barang_masuk_id }}" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="detailModalLabel{{ $d->barang_masuk_id }}">Detail Barang Masuk</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <div class="grid grid-cols-10 gap-2">
+                                                <div class="font-bold col-span-3">Nama Barang:</div>
+                                                <div class="col-span-7">{{ $d->nama_barang }}</div>
+                                                <div class="font-bold col-span-3">Jenis Barang:</div>
+                                                <div class="col-span-7">{{ $d->nama_jenis_barang }}</div>
+                                                <div class="font-bold col-span-3">Supplier:</div>
+                                                <div class="col-span-7">{{ $d->nama_supplier }}</div>
+                                                <div class="font-bold col-span-3">Tanggal Masuk:</div>
+                                                <div class="col-span-7">{{ $d->tanggal }}</div>
+                                                <div class="font-bold col-span-3">Keterangan:</div>
+                                                <div class="col-span-7">{{ $d->keterangan }}</div>
+                                                <div class="font-bold col-span-3">Jumlah:</div>
+                                                <div class="col-span-7">{{ $d->jumlah }}</div>
+
+                                                <!-- Detail barang -->
+                                                @foreach ($d->detail as $index => $detail)
+                                                    <hr class="col-span-10 my-2">
+                                                    <div class="font-bold col-span-3">Barang {{ $index + 1 }}:</div>
                                                     <div class="col-span-7">
-                                                        @foreach ($d['suppliers'] as $supplier => $items)
-                                                            <div class="font-semibold">{{ $supplier }}</div>
-                                                            <ul>
-                                                                @foreach ($items as $item)
-                                                                    <li class="ml-8 list-disc">{{ $item['serial_number'] }} -
-                                                                        <span style="color:{{ $item['warna_status'] }}">{{ $item['nama_status'] }}</span></li>
-                                                                @endforeach
-                                                            </ul>
-                                                        @endforeach
+                                                        <div class="font-bold">SN / Kondisi</div>
+                                                        <div class="ml-5">{{ $detail->serial_number }} — {{ $detail->status_barang }}</div>
+                                                        <div class="font-bold">Kelengkapan</div>
+                                                        <div class="ml-5">{{ $detail->kelengkapan }}</div>
                                                     </div>
-                                                    <div class="font-bold col-span-3">Nama Barang:</div>
-                                                    <div class="col-span-7">{{ $d['nama_barang'] }}</div>
-                                                    <div class="font-bold col-span-3">Jenis Barang:</div>
-                                                    <div class="col-span-7">{{ $d['nama_jenis_barang'] }}</div>
-                                                    <div class="font-bold col-span-3">Jumlah Barang:</div>
-                                                    <div class="col-span-7">{{ $d['jumlah_barang'] }}</div>
-                                                    <div class="font-bold col-span-3">Keterangan:</div>
-                                                    <div class="col-span-7">{{ $d['keterangan_barang'] }}</div>
-                                                </div>
-                                            </div>
-                                            <div class="modal-footer gap-x-3">
-                                                <button type="button" class="!hidden btn btn-secondary"
-                                                    data-bs-dismiss="modal">Tutup</button>
+                                                @endforeach
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            @endforeach
-                        </tbody>
-                    </table>
+                            </div>
+                        @endforeach
+                    </tbody>
+                </table>
 
                     <div class="py-3 px-5">
                         {{ $data->links() }}
